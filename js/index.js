f48925ed282c30779299b4e6974c2524be4f2c51
olvifarrand <olivia.farrand@edu.salpaus.fi>
2023-11-17 09:07:51 +0200

    kaupungeille omat sivut jne

--- js/index.js
+++ js/index.js
@@ -1,21 +1,2 @@
 // 94295a34c9a52d06656a4dbb0881be6f     skymoodsig
 
-async function cityURLhommat() {
-    const APIkey = "94295a34c9a52d06656a4dbb0881be6f";
-
-    const city1 = "Helsinki";
-    const city2 = "Tampere";
-    const city3 = "Vantaa";
-    const city4 = "Lahti";
-    const city5 = "Oulu";
-
-    const queryURL1 = `https://api.openweathermap.org/data/2.5/weather?q=${city1},{+358}&appid=${APIkey}`
-    const queryURL2 = `https://api.openweathermap.org/data/2.5/weather?q=${city2},{+358}&appid=${APIkey}`
-    const queryURL3 = `https://api.openweathermap.org/data/2.5/weather?q=${city3},{+358}&appid=${APIkey}`
-    const queryURL4 = `https://api.openweathermap.org/data/2.5/weather?q=${city4},{+358}&appid=${APIkey}`
-    const queryURL5 = `https://api.openweathermap.org/data/2.5/weather?q=${city5},{+358}&appid=${APIkey}`
-    
-    const response = await fetch(queryURL1);
-    const info = await response.json();
-    console.log(info);
-}
---
+++ js/vantaa.js
@@ -0,0 +1,11 @@
+async function cityURLhommat() {
+    const APIkey = "94295a34c9a52d06656a4dbb0881be6f";
+
+    const city = "Vantaa, FI";
+
+    const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city},{+358}&appid=${APIkey}`
+
+    const response = await fetch(queryURL);
+    const info = await response.json();
+    console.log(info);
+}
